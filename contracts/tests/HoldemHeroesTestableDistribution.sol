2ced3895ce081fc33f209721303db0d895e5a3ff
holdemheroes <github@holdemheroes.com>
2022-03-16 10:41:34 +0000

    contracts - implement CRISP dynamic pricing & reduce contract sizes

--- contracts/tests/HoldemHeroesTestableDistribution.sol
+++ contracts/tests/HoldemHeroesTestableDistribution.sol
@@ -6,7 +6,7 @@ import "../HoldemHeroes.sol";
 
 contract HoldemHeroesTestableDistribution is HoldemHeroes {
 
-    string public constant HAND_PROVENANCE_TESTABLE = "0x4bb5099ae2eaeff17f1e3f6d0006b603440d51f55c13697aa505ecef18ff1f10";
+    bytes32 public constant HAND_PROVENANCE_TESTABLE = 0x4bb5099ae2eaeff17f1e3f6d0006b603440d51f55c13697aa505ecef18ff1f10;
 
     constructor(
         address _vorCoordinator,
@@ -14,9 +14,25 @@ contract HoldemHeroesTestableDistribution is HoldemHeroes {
         address _playingCards,
         uint256 _saleStartTime,
         uint256 _revealSeconds,
-        uint256 _whitelistSeconds,
-        uint256 _maxNfts)
-    HoldemHeroes(_vorCoordinator, _xfund, _playingCards, _saleStartTime, _revealSeconds, _whitelistSeconds, _maxNfts) {}
+        uint256 _maxNfts,
+        int256 _targetBlocksPerSale,
+        int256 _saleHalflife,
+        int256 _priceSpeed,
+        int256 _priceHalflife,
+        int256 _startingPrice)
+    HoldemHeroes(
+        _vorCoordinator,
+        _xfund,
+        _playingCards,
+        _saleStartTime,
+        _revealSeconds,
+        _maxNfts,
+        _targetBlocksPerSale,
+        _saleHalflife,
+        _priceSpeed,
+        _priceHalflife,
+        _startingPrice
+    ){}
 
     /**
      * @dev beginDistributionTestable is used during unit tests to simulate the beginDistribution function

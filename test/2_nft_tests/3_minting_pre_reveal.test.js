f185c52bc35f38858e35f5e6cff6df15a5c58dbd
holdemheroes <github@holdemheroes.com>
2022-03-16 14:46:42 +0000

    test - partially migrated

--- test/2_nft_tests/3_minting_pre_reveal.test.js
+++ test/2_nft_tests/3_minting_pre_reveal.test.js
@@ -7,18 +7,36 @@ const {
 } = require("@openzeppelin/test-helpers")
 
 const { devAddresses, getRanksForUpload } = require( "../helpers/test_data" )
-const { increaseBlockTime } = require("../helpers/chain")
+const { increaseBlockTime, mineOneBlock } = require("../helpers/chain")
 const PlayingCards = artifacts.require("PlayingCards") // Loads a compiled contract
 const HoldemHeroes = artifacts.require("HoldemHeroes") // Loads a compiled contract
 
 contract("HoldemHeroes - pre reveal sale", async function(accounts) {
+
+  const targetBlocksPerSale = 5
+  const saleHalflife = 700
+  const priceSpeed = 1
+  const priceHalflife = 100
+  const startingPrice = web3.utils.toWei("0.22", "ether")
 
   describe('should succeed', function() {
     // deploy contract once before this set of tests
     before(async function () {
       const saleStart = Math.floor(Date.now() / 1000)
       this.playingCards = await PlayingCards.new()
-      this.holdemHeroes = await HoldemHeroes.new(devAddresses.vor, devAddresses.xfund, this.playingCards.address, saleStart, 1000, 0, 5)
+      this.holdemHeroes = await HoldemHeroes.new(
+        devAddresses.vor,
+        devAddresses.xfund,
+        this.playingCards.address,
+        saleStart,
+        1000,
+        5,
+        targetBlocksPerSale,
+        saleHalflife,
+        priceSpeed,
+        priceHalflife,
+        startingPrice
+      )
       await increaseBlockTime(10)
     })
 
@@ -28,7 +46,7 @@ contract("HoldemHeroes - pre reveal sale", async function(accounts) {
       const pricePerNft = await this.holdemHeroes.getNftPrice()
       const cost = pricePerNft.mul(new web3.utils.BN(numToMint))
 
-      const receipt = await this.holdemHeroes.mintNFTPreReveal(numToMint, [], {from: minter, value: cost})
+      const receipt = await this.holdemHeroes.mintNFTPreReveal(numToMint, {from: minter, value: cost})
       expectEvent(receipt, "Transfer", {
         from: constants.ZERO_ADDRESS,
         to: minter,
@@ -38,15 +56,29 @@ contract("HoldemHeroes - pre reveal sale", async function(accounts) {
       expect(nftOwner).to.be.eq(minter)
     })
 
+
+    it("if 0 sent to mintNFTPreReveal, 1 will be minted", async function () {
+      const minter = accounts[1]
+      const pricePerNft = await this.holdemHeroes.getNftPrice()
+      const receipt = await this.holdemHeroes.mintNFTPreReveal(0, {from: minter, value: pricePerNft})
+      expectEvent(receipt, "Transfer", {
+        from: constants.ZERO_ADDRESS,
+        to: minter,
+        tokenId: new BN(1),
+      })
+      const nftOwner = await this.holdemHeroes.ownerOf(1)
+      expect(nftOwner).to.be.eq(minter)
+    })
+
     it("can mintNFTPreReveal max NFTs", async function () {
       const minter = accounts[2]
       const numToMint = 5
       const pricePerNft = await this.holdemHeroes.getNftPrice()
       const cost = pricePerNft.mul(new web3.utils.BN(numToMint))
 
-      const receipt = await this.holdemHeroes.mintNFTPreReveal(numToMint, [], {from: minter, value: cost})
+      const receipt = await this.holdemHeroes.mintNFTPreReveal(numToMint, {from: minter, value: cost})
 
-      for(let i = 1; i <= 5; i += 1) {
+      for(let i = 2; i <= 6; i += 1) {
         expectEvent(receipt, "Transfer", {
           from: constants.ZERO_ADDRESS,
           to: minter,
@@ -59,42 +91,57 @@ contract("HoldemHeroes - pre reveal sale", async function(accounts) {
 
     it("getNftPrice returns correct price per NFT", async function () {
 
+      await mineOneBlock()
+
       const saleStart = Math.floor(Date.now() / 1000)
-      const hh = await HoldemHeroes.new(devAddresses.vor, devAddresses.xfund, this.playingCards.address, saleStart, 1000, 0, 1326)
+      const hh = await HoldemHeroes.new(
+        devAddresses.vor,
+        devAddresses.xfund,
+        this.playingCards.address,
+        saleStart,
+        1000,
+        1326,
+        targetBlocksPerSale,
+        saleHalflife,
+        priceSpeed,
+        priceHalflife,
+        startingPrice
+      )
       await increaseBlockTime(10)
 
-      const minter = accounts[2]
       const numToMint = 50
 
       console.log("this one will take a long time...")
 
       const initialPrice = await hh.getNftPrice()
 
-      // NOTE - adjust schedule according to contract
-      expect( initialPrice ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.001", "ether")) )
+      expect( initialPrice ).to.be.bignumber.eq( new BN(startingPrice) )
 
       for(let i = 0; i < 26; i += 1) {
+        await increaseBlockTime(15)
+        await mineOneBlock()
         const pricePerNft = await hh.getNftPrice()
+        console.log(i, pricePerNft.toString())
         const totalSupply = await hh.totalSupply()
         const cost = pricePerNft.mul(new web3.utils.BN(numToMint))
         process.stdout.write(".")
 
         // NOTE - adjust schedule according to contract
-        if(totalSupply <= 250) {
-          expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.001", "ether")) )
-        } else if(totalSupply <= 500) {
-          expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.002", "ether")) )
-        } else if(totalSupply <= 750) {
-          expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.005", "ether")) )
-        } else if(totalSupply <= 1000) {
-          expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.008", "ether")) )
-        } else {
-          expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.01", "ether")) )
-        }
+        // if(totalSupply <= 250) {
+        //   expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.001", "ether")) )
+        // } else if(totalSupply <= 500) {
+        //   expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.002", "ether")) )
+        // } else if(totalSupply <= 750) {
+        //   expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.005", "ether")) )
+        // } else if(totalSupply <= 1000) {
+        //   expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.008", "ether")) )
+        // } else {
+        //   expect( pricePerNft ).to.be.bignumber.eq( new BN(web3.utils.toWei("0.01", "ether")) )
+        // }
 
         const m = i % accounts.length
 
-        await hh.mintNFTPreReveal( numToMint, [], { from: accounts[m], value: cost })
+        await hh.mintNFTPreReveal( numToMint, { from: accounts[m], value: cost })
 
         // otherwise minters may run out of ETH during est
         await hh.withdrawETH( { from: accounts[0] })
@@ -116,74 +163,140 @@ contract("HoldemHeroes - pre reveal sale", async function(accounts) {
 
     it("cannot mintNFTPreReveal before sale starts", async function () {
       const saleStart = Math.floor(Date.now() / 1000) + 86400
-      const holdemHeroes = await HoldemHeroes.new(devAddresses.vor, devAddresses.xfund, this.playingCards.address, saleStart, 1, 0, 5)
+      const holdemHeroes = await HoldemHeroes.new(
+        devAddresses.vor,
+        devAddresses.xfund,
+        this.playingCards.address,
+        saleStart,
+        1,
+        5,
+        targetBlocksPerSale,
+        saleHalflife,
+        priceSpeed,
+        priceHalflife,
+        startingPrice
+      )
       const pricePerNft = await holdemHeroes.getNftPrice()
       await expectRevert(
-        holdemHeroes.mintNFTPreReveal( 1, [], { from: accounts[1], value: pricePerNft }),
-        "sale not started",
+        holdemHeroes.mintNFTPreReveal( 1, { from: accounts[1], value: pricePerNft }),
+        "not started",
       )
     })
 
     it("cannot mintNFTPreReveal after sale finishes", async function () {
       const saleStart = Math.floor(Date.now() / 1000)
-      const holdemHeroes = await HoldemHeroes.new(devAddresses.vor, devAddresses.xfund, this.playingCards.address, saleStart, 1, 0, 5)
-      await increaseBlockTime(10)
-      const pricePerNft = await holdemHeroes.getNftPrice()
-      await expectRevert(
-        holdemHeroes.mintNFTPreReveal( 1, [], { from: accounts[1], value: pricePerNft }),
-        "pre reveal sale ended",
+      const holdemHeroes = await HoldemHeroes.new(
+        devAddresses.vor,
+        devAddresses.xfund,
+        this.playingCards.address,
+        saleStart,
+        1,
+        5,
+        targetBlocksPerSale,
+        saleHalflife,
+        priceSpeed,
+        priceHalflife,
+        startingPrice
       )
-    })
-
-    it("mintNFTPreReveal must mint > 0", async function () {
-      const saleStart = Math.floor(Date.now() / 1000)
-      const holdemHeroes = await HoldemHeroes.new(devAddresses.vor, devAddresses.xfund, this.playingCards.address, saleStart, 100, 0, 5)
       await increaseBlockTime(10)
       const pricePerNft = await holdemHeroes.getNftPrice()
       await expectRevert(
-        holdemHeroes.mintNFTPreReveal( 0, [], { from: accounts[1], value: pricePerNft }),
-        "cannot buy 0",
+        holdemHeroes.mintNFTPreReveal( 1, { from: accounts[1], value: pricePerNft }),
+        "sale ended",
       )
     })
 
     it("mintNFTPreReveal cannot mint more than max in one tx", async function () {
       const saleStart = Math.floor(Date.now() / 1000)
-      const holdemHeroes = await HoldemHeroes.new(devAddresses.vor, devAddresses.xfund, this.playingCards.address, saleStart, 100, 0, 5)
+      const holdemHeroes = await HoldemHeroes.new(
+        devAddresses.vor,
+        devAddresses.xfund,
+        this.playingCards.address,
+        saleStart,
+        100,
+        5,
+        targetBlocksPerSale,
+        saleHalflife,
+        priceSpeed,
+        priceHalflife,
+        startingPrice
+      )
       await increaseBlockTime(10)
       const pricePerNft = await holdemHeroes.getNftPrice()
       await expectRevert(
-        holdemHeroes.mintNFTPreReveal( 6, [], { from: accounts[1], value: pricePerNft }),
-        "max 5 per tx",
+        holdemHeroes.mintNFTPreReveal( 6, { from: accounts[1], value: pricePerNft }),
+        "> max per tx",
       )
     })
 
     it("mintNFTPreReveal cannot own more than max", async function () {
       const saleStart = Math.floor(Date.now() / 1000)
-      const holdemHeroes = await HoldemHeroes.new(devAddresses.vor, devAddresses.xfund, this.playingCards.address, saleStart, 100, 0, 5)
+      const holdemHeroes = await HoldemHeroes.new(
+        devAddresses.vor,
+        devAddresses.xfund,
+        this.playingCards.address,
+        saleStart,
+        100,
+        5,
+        targetBlocksPerSale,
+        saleHalflife,
+        priceSpeed,
+        priceHalflife,
+        startingPrice
+      )
       await increaseBlockTime(10)
       const numToMint = 5
       const pricePerNft = await holdemHeroes.getNftPrice()
       const cost = pricePerNft.mul(new web3.utils.BN(numToMint))
-      await holdemHeroes.mintNFTPreReveal( numToMint, [], { from: accounts[1], value: cost })
+      await holdemHeroes.mintNFTPreReveal( numToMint, { from: accounts[1], value: cost })
       await expectRevert(
-        holdemHeroes.mintNFTPreReveal( 1, [], { from: accounts[1], value: pricePerNft }),
+        holdemHeroes.mintNFTPreReveal( 1, { from: accounts[1], value: pricePerNft }),
         "mint limit reached",
       )
     })
 
     it("mintNFTPreReveal must send correct value", async function () {
+      await mineOneBlock()
+      
       const saleStart = Math.floor(Date.now() / 1000)
-      const holdemHeroes = await HoldemHeroes.new(devAddresses.vor, devAddresses.xfund, this.playingCards.address, saleStart, 100, 0, 5)
-      await increaseBlockTime(10)
+      const holdemHeroes = await HoldemHeroes.new(
+        devAddresses.vor,
+        devAddresses.xfund,
+        this.playingCards.address,
+        saleStart,
+        100,
+        5,
+        targetBlocksPerSale,
+        saleHalflife,
+        priceSpeed,
+        priceHalflife,
+        startingPrice
+      )
+
+      const pricePerNft = await holdemHeroes.getNftPrice()
+      console.log("pricePerNft", pricePerNft.toString())
+
       await expectRevert(
-        holdemHeroes.mintNFTPreReveal( 1, [], { from: accounts[1], value: 100 }),
+        holdemHeroes.mintNFTPreReveal( 1, { from: accounts[1], value: 1 }),
         "eth value incorrect",
       )
     })
 
     it("cannot mintNFTPreReveal more than max supply", async function () {
       const saleStart = Math.floor(Date.now() / 1000)
-      const holdemHeroes = await HoldemHeroes.new(devAddresses.vor, devAddresses.xfund, this.playingCards.address, saleStart, 86400, 0, 1326)
+      const holdemHeroes = await HoldemHeroes.new(
+        devAddresses.vor,
+        devAddresses.xfund,
+        this.playingCards.address,
+        saleStart,
+        86400,
+        1326,
+        targetBlocksPerSale,
+        saleHalflife,
+        priceSpeed,
+        priceHalflife,
+        startingPrice
+      )
       await increaseBlockTime(10)
 
       const numToMint = 50
@@ -195,7 +308,7 @@ contract("HoldemHeroes - pre reveal sale", async function(accounts) {
         const cost = pricePerNft.mul(new web3.utils.BN(numToMint))
         process.stdout.write(".")
         const m = i % accounts.length
-        await holdemHeroes.mintNFTPreReveal( numToMint, [], { from: accounts[m], value: cost })
+        await holdemHeroes.mintNFTPreReveal( numToMint, { from: accounts[m], value: cost })
 
         // otherwise minters may run out of ETH during est
         await holdemHeroes.withdrawETH( { from: accounts[0] })
@@ -207,18 +320,18 @@ contract("HoldemHeroes - pre reveal sale", async function(accounts) {
       const finalMint = 25
       const pricePerNft = await holdemHeroes.getNftPrice()
       const finalCost = pricePerNft.mul(new web3.utils.BN(finalMint))
-      await holdemHeroes.mintNFTPreReveal( finalMint, [], { from: accounts[1], value: finalCost })
+      await holdemHeroes.mintNFTPreReveal( finalMint, { from: accounts[1], value: finalCost })
 
       await expectRevert(
-        holdemHeroes.mintNFTPreReveal( finalMint, [], { from: accounts[1], value: finalCost }),
-        "exceeds MAX_NFT_SUPPLY",
+        holdemHeroes.mintNFTPreReveal( finalMint, { from: accounts[1], value: finalCost }),
+        "exceeds supply",
       )
 
       // mint last one
-      await holdemHeroes.mintNFTPreReveal( 1, [], { from: accounts[1], value: pricePerNft })
+      await holdemHeroes.mintNFTPreReveal( 1, { from: accounts[1], value: pricePerNft })
 
       await expectRevert(
-        holdemHeroes.mintNFTPreReveal( 1, [], { from: accounts[1], value: pricePerNft }),
+        holdemHeroes.mintNFTPreReveal( 1, { from: accounts[1], value: pricePerNft }),
         "sold out",
       )
     })

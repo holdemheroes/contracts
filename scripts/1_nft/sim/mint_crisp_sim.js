6716b73244b5353e8ddc6dfa4e2afbfb81c7dfe5
rdz108 <82244855+rdz108@users.noreply.github.com>
2022-03-19 14:40:23 +0800

    Update mint_crisp_sim.js

--- scripts/1_nft/sim/mint_crisp_sim.js
+++ scripts/1_nft/sim/mint_crisp_sim.js
@@ -8,7 +8,7 @@ module.exports = async function(callback) {
   // ----------------------------------
 
   // CRISP specific
-  const targetBlocksPerSale = 3 // Ideal time between mints
+  const targetBlocksPerSale = 2 // Ideal time between mints
   const saleHalflife = 94     // CRISP example sets to 700
   const priceSpeed = 1           // CRISP example sets to 1
   const priceHalflife = 94     // CRISP example sets to 100

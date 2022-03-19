5b76d933af36947a3140ddd0049a10cfb79ea4ce
rdz108 <82244855+rdz108@users.noreply.github.com>
2022-03-19 14:27:14 +0800

    Update mint_crisp_sim.js

--- scripts/1_nft/sim/mint_crisp_sim.js
+++ scripts/1_nft/sim/mint_crisp_sim.js
@@ -9,9 +9,9 @@ module.exports = async function(callback) {
 
   // CRISP specific
   const targetBlocksPerSale = 3 // Ideal time between mints
-  const saleHalflife = 3     // CRISP example sets to 700
+  const saleHalflife = 6     // CRISP example sets to 700
   const priceSpeed = 1           // CRISP example sets to 1
-  const priceHalflife = 9     // CRISP example sets to 100
+  const priceHalflife = 36     // CRISP example sets to 100
   const startingPrice = 0.1    // Start price in ETH. Will be converted to wei in the script
 
   // Simulation variables

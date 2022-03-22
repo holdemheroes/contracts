6ffb87625522de19a1760794948a012452ca760d
holdemheroes <github@holdemheroes.com>
2022-03-22 13:50:57 +0000

    migrations - update deployment params

--- migrations/2_deploy_heh.js
+++ migrations/2_deploy_heh.js
@@ -8,16 +8,16 @@ module.exports = function(deployer, network) {
   deployer.then(async () => {
 
     // TODO - SET THESE VALUES FOR TARGET NETWORK
-    const saleStart = Math.floor(Date.now() / 1000) + 18000 // 3 hours from now
-    const timeUntilReveal = 180000 // 3 hours for Rinkeby
+    const saleStart = Math.floor(Date.now() / 1000) //+ 18000 // 3 hours from now
+    const timeUntilReveal = 86400 // 3 hours for Rinkeby
     const maxNftsPerTxOrAddress = 1326 // 1326 for Rinkeby/Vordev ONLY. For MN, use 6
 
     // CRISP
-    const targetBlocksPerSale = 5
-    const saleHalflife = 700
+    const targetBlocksPerSale = 9
+    const saleHalflife = 81
     const priceSpeed = 1
-    const priceHalflife = 100
-    const startingPrice = web3.utils.toWei("0.22", "ether")
+    const priceHalflife = 81
+    const startingPrice = web3.utils.toWei("0.01", "ether")
 
     await deployer.deploy(
       HoldemHeroes,

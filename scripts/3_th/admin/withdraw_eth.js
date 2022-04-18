e1a546543b57a1ddf329b4c17b8dfc9f942e59ed
holdemheroes <github@holdemheroes.com>
2022-04-18 11:39:18 +0100

    separate mn/tn

--- scripts/3_th/admin/withdraw_eth.js
+++ scripts/3_th/admin/withdraw_eth.js
@@ -6,7 +6,14 @@ const Web3 = require("web3")
 const TexasHoldemV1Abi = require('../../../data/abis/TexasHoldemV1.json')
 const contractAddresses = require("../../../data/contractAddresses.json")
 
-const { ETH_PKEY, ALCHEMY_KEY } = process.env
+const {
+  ETH_PKEY_TN,
+  ETH_PKEY_MN,
+  ALCHEMY_KEY_ETH_MN,
+  ALCHEMY_KEY_POLY_MN,
+  ALCHEMY_KEY_ETH_TN,
+  ALCHEMY_KEY_POLY_TN
+} = process.env
 
 args
   .option("network", "The network to deal on")
@@ -28,25 +35,41 @@ if(!validNetworks.includes(flags.network)) {
 }
 
 let alchemyPrefix = ""
+let alchemyKey = ""
+let pKey = ""
 
 switch(flags.network) {
   case "rinkeby":
     alchemyPrefix = "eth-rinkeby.alchemyapi.io/v2"
+    pKey = ETH_PKEY_TN
+    alchemyKey = ALCHEMY_KEY_ETH_TN
     break
   case "polygon_mumbai":
     alchemyPrefix = "polygon-mumbai.g.alchemy.com/v2"
+    pKey = ETH_PKEY_TN
+    alchemyKey = ALCHEMY_KEY_POLY_TN
+    break
+  case "mainnet":
+    alchemyPrefix = "eth-mainnet.alchemyapi.io/v2"
+    pKey = ETH_PKEY_MN
+    alchemyKey = ALCHEMY_KEY_ETH_MN
+    break
+  case "polygon":
+    alchemyPrefix = "polygon-mainnet.g.alchemy.com/v2"
+    pKey = ETH_PKEY_MN
+    alchemyKey = ALCHEMY_KEY_POLY_MN
     break
   default:
     console.log("not supported yet")
     process.exit(1)
 }
 
-const url = `https://${alchemyPrefix}/${ALCHEMY_KEY}`
+const url = `https://${alchemyPrefix}/${alchemyKey}`
 
 async function withdraw() {
 
   const contractAddr = contractAddresses[flags.network].texas_holdem_v1
-  const providerHttp = new HDWalletProvider(ETH_PKEY, url)
+  const providerHttp = new HDWalletProvider(pKey, url)
   const web3Http = await new Web3(providerHttp)
   const contractHttp = await new web3Http.eth.Contract(TexasHoldemV1Abi, contractAddr)
 

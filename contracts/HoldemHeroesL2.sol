0c721f55707a89f3a6fabdc921665b883df38623
holdemheroes <github@holdemheroes.com>
2022-04-25 11:39:10 +0100

    allow minting on L2

--- contracts/HoldemHeroesL2.sol
+++ contracts/HoldemHeroesL2.sol
@@ -10,6 +10,8 @@ contract HoldemHeroesL2 is Ownable, HoldemHeroesBase  {
     address public parentChainContractAddress;
     uint256 public parentNetworkId;
 
+    uint256 public basePostRevealPrice;
+
     /**
      * @dev constructor
      * @dev initialises some basic variables.
@@ -20,14 +22,19 @@ contract HoldemHeroesL2 is Ownable, HoldemHeroesBase  {
      * @param _playingCards address - address of Playing Cards contract
      */
     constructor(uint256 _startingIndex, address _parentChainContractAddress, uint256 _parentNetworkId, address _playingCards)
-    // There is no sale/pre-reveal sale on L2. Just using airdrop
+    // There is no pre-reveal sale on L2. Just using airdrop & post-reveal mint
     HoldemHeroesBase(0, _playingCards)
     {
         REVEAL_TIMESTAMP = block.timestamp; // no reveal time
-        startingIndex = _startingIndex; // already distributed on parent chain.
+        startingIndex = _startingIndex; // already distributed on parent chain. Use same start index
         parentChainContractAddress = _parentChainContractAddress;
         parentNetworkId = _parentNetworkId;
+        basePostRevealPrice = 150 ether; // 150 MATIC
     }
+
+    /**
+     * ADMIN
+     */
 
     /**
      * @dev airdrop is an admin function to mint L1 tokens for their respective owners on L2.
@@ -41,5 +48,51 @@ contract HoldemHeroesL2 is Ownable, HoldemHeroesBase  {
             require(_owners[i] != address(0), "invalid owner");
             _safeMint(_owners[i], _tokenIds[i]);
         }
+    }
+
+    /**
+     * @dev setBasePostRevealPrice allows owner to adjust post-reveal price according to market
+     *
+     * @param newPrice uint256 new base price in wei
+     */
+    function setBasePostRevealPrice(uint256 newPrice) external onlyOwner {
+        basePostRevealPrice = newPrice;
+    }
+
+    /**
+     * MINT
+     */
+
+    /**
+     * @dev getPostRevealNftPrice get mint price for revealed tokens, based on the hand Rank
+     * @dev lower rank = better hand = higher price. e.g. AA = rank 1 = high price
+     * @dev Note - reveal and distribution occurred on L1 Eth mainnet. Hands and start index
+     * @dev are ported to the L2 contract, and are identical to the HoldemHeroes contract on L1.
+     *
+     * @return result uint256 price in wei
+     */
+    function getPostRevealNftPrice(uint256 _tokenId) public view returns (uint256 result) {
+        uint256 rank = uint256(getHandRank(tokenIdToHandId(_tokenId)));
+        if(rank == 1) {
+            result = basePostRevealPrice;
+        } else {
+            uint256 m = 100 - ((rank * 100) / 169); // get % as int
+            m = (m < 10) ? 10 : m;
+            result = (basePostRevealPrice * m) / 100;
+        }
+    }
+
+    /**
+     * @dev mintNFTPostReveal is a public payable function which any user can call.
+     * @dev This allows a user to mint any available token ID that hasn't been sold yet.
+     * @dev Correct ether value is expected to pay for token.
+     *
+     * @param tokenId uint256 NFT Token ID to purchase
+     */
+    function mintNFTPostReveal(uint256 tokenId) external payable {
+        uint256 price = getPostRevealNftPrice(tokenId);
+        require(msg.value >= price, "eth too low");
+
+        _safeMint(msg.sender, tokenId);
     }
 }
